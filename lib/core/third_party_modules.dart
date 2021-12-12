6ea92ef7e23c818c5059637d008b4045d4e73ba8
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2021-12-12 06:19:23 +0330

    change base url of dio.

--- lib/core/third_party_modules.dart
+++ lib/core/third_party_modules.dart
@@ -1,11 +1,12 @@
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
 
 @module
 abstract class RegisterModule {
   @Named("BaseUrl")
-  String get baseUrl => 'My base url';
+  String get baseUrl => dotenv.env['BASE_URL']!;
 
   @lazySingleton
   Dio dio(@Named('BaseUrl') String url) => Dio(BaseOptions(baseUrl: url));
